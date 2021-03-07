4dbe224c864208de98fe5aef83fc093e44ac9ea0
Ramin Afandiyev <raminefendi@gmail.com>
2021-03-07 15:06:31 +0400

    Cors problem solved

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
