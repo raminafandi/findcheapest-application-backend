ef941d4068e0592f967b9b62414f76271c9ad861
Ramin Afandiyev <raminefendi@gmail.com>
2021-03-22 22:58:01 +0400

    Update profile and Change Password routes added into auth.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,15 +6,12 @@ const config = require("config");
 const User = require("../../models/User");
 const auth = require("../../middleware/auth");
 
-router.get("/myprofile", auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select("-password");
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
+/* 
+POST Method : /api/auth/register
+Registers the user
+Returns : token for authentication
+no need to be authenticated
+*/
 
 router.post("/register", async (req, res) => {
   const { email, password, password2, username } = req.body;
@@ -71,6 +68,13 @@ router.post("/register", async (req, res) => {
   }
 });
 
+/* 
+POST Method : /api/auth/Login
+Login the user
+Returns : token for authentication
+no need to be authenticated
+*/
+
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
@@ -112,6 +116,85 @@ router.post("/login", async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
+  }
+});
+
+/* 
+GET Method : /api/auth/myprofile
+Gets the user information
+Returns : user info as object without password field
+should be authenticated
+*/
+
+router.get("/myprofile", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/* 
+/api/auth/updateprofile
+Updates the user information
+Returns: updated user information
+should be authenticated
+*/
+router.put("/updateprofile", auth, async (req, res) => {
+  const { emailNew, usernameNew } = req.body;
+  try {
+    let user = await User.findById(req.user.id).select("-password");
+    emailNew ? (user.email = emailNew) : null;
+    usernameNew ? (user.username = usernameNew) : null;
+    await user.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/* 
+/api/auth/changepassword
+Updates the user password
+Returns: updated user information
+should be authenticated
+*/
+router.put("/changepassword", auth, async (req, res) => {
+  const { passwordOld, passwordNew, passwordNew2 } = req.body;
+  try {
+    let user = await User.findById(req.user.id);
+    const salt = await bcrypt.genSalt(10);
+
+    const isMatch = await bcrypt.compare(passwordOld, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        errors: [
+          {
+            msg: "Old Password is no correct.",
+          },
+        ],
+      });
+    }
+
+    if (passwordNew === passwordNew2) {
+      user.password = await bcrypt.hash(passwordNew, salt);
+      await user.save();
+    } else {
+      return res.status(400).json({
+        errors: [
+          {
+            msg: "Passwords are not matching.",
+          },
+        ],
+      });
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
   }
 });
 
