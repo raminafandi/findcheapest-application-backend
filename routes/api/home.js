57e9eecd4f8545851c3b2c77f4db7c05dcbd26d8
Rasim <rasimmammadoff1@gmail.com>
2021-03-22 11:22:39 -0700

    Check ip

--- routes/api/home.js
+++ routes/api/home.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 
-router.get("/", auth, async (req, res) => {
+
+router.get("/", async (req, res) => {
   try {
-    res.json({ neymar: "home" });
+    res.json({ aganeymar: req.ip });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
